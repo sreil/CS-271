cefd806eb897a32d34561baedc7c061489abd4e3
sreil <seanpreilly@gmail.com>
2015-06-25 09:50:54 -0600

    test

---
+++ Week 2/Program 1/Program1.asm
@@ -0,0 +1,26 @@
+TITLE Program Template			(template.asm)
+
+; Program Description: Display your name and program title, display instructions, enter 2 numbers, calculate sum, difference, product, and quotient, display terminating message
+; Author: Sean Reilly
+; Date Created: 06/24/2015
+; Last Modification Date: 06/24/2015
+
+INCLUDE Irvine32.inc
+
+; (insert symbol definitions here)
+
+.data
+
+; (insert variables here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	exit		; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
