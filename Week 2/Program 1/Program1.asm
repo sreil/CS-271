0080deef2b14b22761c1c591757aa660d87dd79c
sreil <seanpreilly@gmail.com>
2015-06-28 06:24:53 -0600

    week 1 summary

--- Week 2/Program 1/Program1.asm
+++ Week 2/Program 1/Program1.asm
@@ -11,7 +11,23 @@ INCLUDE Irvine32.inc
 
 .data
 
-; (insert variables here)
+intro		BYTE	"Program 1 by Sean Reilly"	;displays my name
+intro2		BYTE	"Enter here"				;CHANGE THIS
+instruction	BYTE	"This will take 2 numbers that you enter and calcualte the sum, difference, product and quotient", 0
+number1		DWORD	"Please enter the first number: "
+number2		DWORD	"Please enter the second number: "
+sum			DWORD	?							;calcualtes sum of number 1 and number 2
+difference	DWORD	?							;calculates the difference of number 1 and number 2
+product		DWORD	?							;calculates the product of number 1 and number 2
+quotient	DWORD	?							;calculates the quotient of number 1 and number 2
+remainder	DWORD	?							;calculates the remainder of the quotient of number 1 and number 2
+result		BYTE	"The results are the following: ", 0
+sumRes		BYTE	"Sum: ", 0
+difRes		BYTE	"Difference: ", 0
+prodRes		BYTE	"Product: ", 0
+quotRes		BYTE	"Quotient: ", 0
+remRes		BYTE	"The quotient has a remainder of: ", 0
+terminate	BYTE	"That was fun! Adios!", 0
 
 .code
 main PROC
