0243e8c6f3d0f750e60db493bc80870ef00fb84c
sreil <seanpreilly@gmail.com>
2015-08-05 06:27:10 -0600

    added program 5 beginning

---
+++ Week 7/Program5.asm
@@ -0,0 +1,26 @@
+TITLE Program5			(Program5.asm)
+
+; Program Description:
+; Author: Sean Reilly
+; Date Created: 8/4/15
+; Last Modification Date: 8/4/15
+
+INCLUDE Irvine32.inc
+
+; (insert symbol definitions here)
+
+.data
+
+; (insert variables here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	exit		; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
