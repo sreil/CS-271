75bd9798b0f68f15d0d901f52ef70d983922ee4a
sreil <seanpreilly@gmail.com>
2015-08-05 18:24:43 -0600

    added prompt/instructions

--- Week 7/Program5.asm
+++ Week 7/Program5.asm
@@ -9,9 +9,18 @@ INCLUDE Irvine32.inc
 
 ; (insert symbol definitions here)
 
+
 .data
 
-; (insert variables here)
+instruct				BYTE	"Welcome to the Combinations Calculator", 0
+instruct2				BYTE	"     Implemented by Sean Reilly", 0
+instruct3				BYTE	"I'll give you a combinations problem. You enter your answer,", 0
+instruct4				BYTE	"and I'll let you know if you're right.", 0
+prompt					BYTE	"Problem:", 0
+prompt2					BYTE	"Number of elements in the set: ", 0
+prompt3					BYTE	"Number of elements to choose from the set: ", 0
+prompt4					BYTE	"How many ways can you choose? ", 0
+
 
 .code
 main PROC
